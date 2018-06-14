a63101713363820654051d2b692e7adbe0725c2e
ajschilt <ajschilt86@gmail.com>
2018-06-14 11:38:17 -0500

    draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var userArray = [];
+var compChoice = letters[Math.floor(Math.random() * letters.length)]
+
+console.log(compChoice);
+
+
+document.addEventListener("keydown", function (event) {
+    var userGuess = event.key;
+
+
+    if (userGuess === compChoice) {
+        wins++;
+
+    }
+    else {
+        guesses--;
+    }
+
+    if (guesses === 0) {
+        losses++, 
+        guesses = 10;
+    
+    }
+
+    userArray.push(userGuess);
+
+    console.log(event)
+
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "losses: " + losses;
+    document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
+    document.getElementById("lettersGuessed").innerHTML = userArray;
+
+
+})
+
+// for (i = 0; i < 11; i++) { //
+
+
+
+document.getElementById("wins").innerHTML = "Wins: " + wins;
+document.getElementById("losses").innerHTML = "losses: " + losses;
+document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
+document.getElementById("lettersGuessed").innerHTML = userArray;
